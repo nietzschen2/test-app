52dbc24145566694e01bb04ba11e20f725828ce3
Alastair <alastairclegg@Alastairs-MacBook-Pro-2.local>
2017-06-20 13:55:04 +0100

    Return to stability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,6 @@ import { MaterialToolbarComponent } from './material-toolbar/material-toolbar.co
 import { PlaceholderCardsComponent } from './placeholder-cards/placeholder-cards.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
 import { NormalButtonComponent } from './normal-button/normal-button.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { BasicHeaderComponent } from './basic-header/basic-header.component';
 
 @NgModule({
   declarations: [
@@ -21,9 +19,7 @@ import { BasicHeaderComponent } from './basic-header/basic-header.component';
     MaterialToolbarComponent,
     PlaceholderCardsComponent,
     DatepickerComponent,
-    NormalButtonComponent,
-    LoginFormComponent,
-    BasicHeaderComponent
+    NormalButtonComponent
   ],
   imports: [
     BrowserModule,
